d68db4665ec9a6beecb494ebff509607914051b6
ryuhei202 <ryuhei202@docomo.ne.jp>
2022-12-08 22:59:50 +0900

    Get data for selected prefectures

--- src/apis/population.ts
+++ src/apis/population.ts
@@ -5,7 +5,7 @@ import { prefecturePopuration } from "../urls";
 export const fetchPopulation = (prefCode:number) => {
   return axios.get(prefecturePopuration(prefCode),{ headers: { 'X-API-KEY': '	2d7e9nRWrZGKkIikLRayeL7gKjiJZIsXbUDRb98Z' } })
   .then(res => {
-   console.log(res);
+    return res;
   })
   .catch((e) => console.error(e))
 }
