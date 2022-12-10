3f60263f1d1db7feb72103c8e18d7591d07024f5
ryuhei202 <ryuhei202@docomo.ne.jp>
2022-12-10 19:05:57 +0900

    delete 404page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import { Page404 } from './containers/Pages/Page404';
 import { TopPage } from './containers/Pages/TopPage';
 
 function App() {
 
 return (
   <>
-    <BrowserRouter>
-      <Routes>
-       <Route path='/' element={<TopPage />} />
-       <Route path='*' element={<Page404 />} />
-     </Routes>
-    </BrowserRouter>
+    <TopPage /> 
   </>
 )
 }
